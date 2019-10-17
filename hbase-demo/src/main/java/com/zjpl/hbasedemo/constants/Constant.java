df3dbdb8cf3c05c433e068a9d009001b7485a7d6
luckystaryu <465078693@qq.com>
2019-10-18 07:51:56 +0800

    2019101807

--- hbase-demo/src/main/java/com/zjpl/hbasedemo/constants/Constant.java
+++ hbase-demo/src/main/java/com/zjpl/hbasedemo/constants/Constant.java
@@ -6,9 +6,26 @@ package com.zjpl.hbasedemo.constants;
  * Description
  */
 public class Constant {
+
+    /**
+     * 微博内容表
+     */
     public static final String WEIBO_CONTENT="ZJPL:weibo_content";
-    public static final String COLUMN_FAMILY_CONTENT = "info";
+    public static final String CONTENT_CF = "info";
     public static final Integer MAXVERSION_CONTENT= 1;
 
-    //private static final String WEIBO_
+    /**
+     * 用户关系表
+     */
+    public static final String USER_RELATION="ZJPL:user_relation";
+    public static final String USER_RELATION_CF1 ="attends";
+    public static final String USER_RELATION_CF2 ="fans";
+    public static final Integer MAXVERSION_USER_RELATION= 1;
+
+    /**
+     * 收件箱
+     */
+    public static final String INBOX ="ZJPL:inbox";
+    public static final String INBOX_CF ="info";
+    public static final Integer MAXVERSION_INBOX= 2;
 }
--- hbase-demo/src/main/java/com/zjpl/hbasedemo/dao/weiboDao.java
+++ hbase-demo/src/main/java/com/zjpl/hbasedemo/dao/weiboDao.java
@@ -2,7 +2,12 @@ package com.zjpl.hbasedemo.dao;
 
 import com.zjpl.hbasedemo.constants.Constant;
 import com.zjpl.hbasedemo.utils.HBaseUtils;
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.CellUtil;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.HTable;
 import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.IOException;
@@ -18,21 +23,36 @@ import java.util.List;
  * 3.取关用户
  */
 public class weiboDao {
-    public static void publisWeiBo(String uid, String content){
+    public static void publisWeiBo(String uid, String content) throws IOException {
+        //获取时间戳
+        long ts = System.currentTimeMillis();
+        //获取RowKey
+        String rowKey = uid+"_"+ts;
         String CONTENT_COLUMN ="content";
         try {
-            HBaseUtils.getInstance().putData(Constant.WEIBO_CONTENT,uid,Constant.COLUMN_FAMILY_CONTENT,CONTENT_COLUMN,content);
+            HBaseUtils.getInstance().putData(Constant.WEIBO_CONTENT,rowKey,Constant.CONTENT_CF,CONTENT_COLUMN,content);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        //创建一个集合，用于存放微博内容表的对象
+
+        //第二步操作微博收件箱
+        Result result = HBaseUtils.getInstance().getDataByRowKey(Constant.USER_RELATION, uid);
+        for (Cell columnCell : result.rawCells()) {
+
         }
     }
     public static void main(String[] args) throws IOException {
         List<String> list = new ArrayList<>();
-        list.add(Constant.COLUMN_FAMILY_CONTENT);
+        list.add(Constant.CONTENT_CF);
         HBaseUtils.getInstance().createTable(Constant.WEIBO_CONTENT,list,Constant.MAXVERSION_CONTENT,false);
         //publisWeiBo("zhangsan","我发微博了！");
         List<Result> listResult= HBaseUtils.getInstance().getAllData(Constant.WEIBO_CONTENT);
         for (Result result : listResult) {
+            for (Cell cell : result.rawCells()) {
+                System.out.println(CellUtil.cloneRow(cell));
+            }
         }
     }
 }
